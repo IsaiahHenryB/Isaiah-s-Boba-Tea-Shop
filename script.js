8963e018b714fab2dd20dc0331a6b3030b98e664
IsaiahHenry <isaiahhenryb@gmail.com>
2021-03-08 21:15:23 -0500

    added some JS to this old shop

---
+++ script.js
@@ -0,0 +1,16 @@
+let dName = prompt("What drink do you want");
+
+let size = prompt("What size do you want");
+
+let sugar = prompt("How much sugar do you want");
+
+let ice = prompt("How much ice do you want");
+
+let toppings = prompt("What topping do you want");
+
+document.getElementById("dName").innerHTML = dName;
+
+document.getElementById("size").innerHTML = size;
+document.getElementById("sugar").innerHTML = sugar;
+document.getElementById("ice").innerHTML = ice;
+document.getElementById("toppings").innerHTML = toppings;
